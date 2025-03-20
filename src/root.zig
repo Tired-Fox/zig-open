a4169765eded567daf4fc3e390d0472ab1e88adf
Zachary Boehm <zboehm@menard-inc.com>
2025-03-20 17:23:55 -0500

    [feat] fix windows ampersand escape

--- src/root.zig
+++ src/root.zig
@@ -7,8 +7,15 @@ pub fn that(path: []const u8) !void {
     defer arena.deinit();
     const allo = arena.allocator();
 
+    std.debug.print("{s}\n", .{path});
+
     switch (@import("builtin").os.tag) {
         .windows => {
+            const size = std.mem.replacementSize(u8, path, "&", "^&");
+            const target = try allo.alloc(u8, size);
+            defer allo.free(target);
+            _ = std.mem.replace(u8, path, "&", "^&", target);
+
             _ = std.process.Child.run(.{
                 .allocator = allo,
                 .argv = &.{
@@ -16,7 +23,7 @@ pub fn that(path: []const u8) !void {
                     "/c",
                     "start",
                     "\"\"",
-                    path
+                    target
                 }
             }) catch return error.NoWorkingLauncher;
         },
@@ -44,6 +51,56 @@ pub fn that(path: []const u8) !void {
             } else {
                 return error.NoWorkingLauncher;
             }
+        },
+        else => |platform| @compileError("unsupported platform: " ++ @tagName(platform))
+    }
+}
+
+/// Attempt to open the path in the default application
+/// given the system launcher.
+pub fn with(app: []const u8, path: []const u8) !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allo = arena.allocator();
+
+    switch (@import("builtin").os.tag) {
+        .windows => {
+            const size = std.mem.replacementSize(u8, path, "&", "^&");
+            const target = try allo.alloc(u8, size);
+            defer allo.free(target);
+            _ = std.mem.replace(u8, path, "&", "^&", target);
+
+            _ = std.process.Child.run(.{
+                .allocator = allo,
+                .argv = &.{
+                    "cmd",
+                    "/c",
+                    "start",
+                    "\"\"",
+                    app,
+                    path
+                }
+            }) catch return error.NoWorkingLauncher;
+        },
+        .macos => {
+            _ = std.process.Child.run(.{
+                .allocator = allo,
+                .argv = &.{
+                    "/usr/bin/open",
+                    path,
+                    "-a",
+                    app
+                }
+            }) catch return error.NoWorkingLauncher;
+        },
+        .linux => {
+            _ = std.process.Child.run(.{
+                .allocator = allo,
+                .argv = &.{
+                    app,
+                    path,
+                }
+            }) catch return error.NoWorkingLauncher;
         },
         else => |platform| @compileError("unsupported platform: " ++ @tagName(platform))
     }
